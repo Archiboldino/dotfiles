735f8955907a0b8aa3fb1ae0ace3558bc3552ec6
Nikita Zabaikin <nikita.zabaikin@wickes.co.uk>
2025-07-03 19:16:15 +0300

    Update .config/waybar/scripts/waybar-wttr.py

--- private_dot_config/waybar/scripts/executable_waybar-wttr.py
+++ private_dot_config/waybar/scripts/executable_waybar-wttr.py
@@ -96,7 +96,7 @@ if tempint > 0 and tempint < 10:
 data['text'] = ''+WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
     " "+extrachar+weather['current_condition'][0]['FeelsLikeC']+"°"
 
-data['tooltip'] = f"<b>{datetime.now().strftime('%H:%M %A %d %B %Y')}</b>\n"
+data['tooltip'] = f"<b>{weather['current_condition'][0]['localObsDateTime']}</b>\n"
 data['tooltip'] += f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
 data['tooltip'] += f"Feels like: {weather['current_condition'][0]['FeelsLikeC']}°\n"
 data['tooltip'] += f"Wind: {weather['current_condition'][0]['windspeedKmph']}Km/h\n"
