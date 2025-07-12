62237e4edd999554c5c063a5afb751b844300fd9
Nikita Zabaikin <nikita.zabaikin@wickes.co.uk>
2025-07-12 16:36:22 +0300

    vim update

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -25,6 +25,7 @@ return {
       },
     },
   },
+
   -- TS
   { "haydenmeade/neotest-jest" },
   {
@@ -37,28 +38,26 @@ return {
       },
     },
   },
+
   -- Rust
-  {
-    "mrcjkb/rustaceanvim",
-    opts = {
-      server = {
-        default_settings = {
-          -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {
-            server = {
-              -- extraEnv = { {["RA_LOG"]="project_model=debug"} }, -- For debugging rust-analyzer, to see log location do :LspInfo in neovim
-            },
-            cargo = {
-              allFeatures = false, -- important
-              extraArgs = { "--release" }, -- probably not required, but better since used for building
-              allTargets = false, -- Not required
-              -- target = "xtensa-esp32-none-elf", -- can be avoided if using .cargo/config.toml as described below. CAN'T be a list even if in rust-analyzer doc it says otherwise
-            },
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "mrcjkb/rustaceanvim",
+  --   opts = {
+  --     server = {
+  --       default_settings = {
+  --         -- rust-analyzer language server configuration
+  --         ["rust-analyzer"] = {
+  --           cargo = {
+  --             -- allFeatures = false, -- important
+  --             -- extraArgs = { "--release" }, -- probably not required, but better since used for building
+  --             allTargets = false, -- Not required
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
+
   -- YAML
   {
     "neovim/nvim-lspconfig",
