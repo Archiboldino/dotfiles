6364500a7136dd738986583026ff4402254c0921
Nikita Zabaikin <nikita.zabaikin@wickes.co.uk>
2026-08-08 21:22:54 +0300

    nvm, mon

--- private_dot_config/fish/functions/_nvm_check.fish
+++ private_dot_config/fish/functions/_nvm_check.fish
@@ -1,12 +1,12 @@
-function _nvm_check --on-variable PWD --description 'Do nvm stuff'
+function _nvm_check --on-variable PWD --description 'Autoload node version from .nvmrc'
     if test -f .nvmrc
-        set node_version (nvm current)
-        set nvmrc_node_version (nvm list (cat .nvmrc))
-
-        if [ $status = 1 ]
-            nvm install
-        else if [ $nvmrc_node_version != $node_version ]
-            nvm use
+        set -l nvmrc_version (string trim (cat .nvmrc))
+        set -l current_version (node -v 2>/dev/null | string trim -l -c v)
+        if test "$nvmrc_version" != "$current_version"
+            if not nvm use $nvmrc_version >/dev/null 2>&1
+                nvm install $nvmrc_version >/dev/null 2>&1
+                echo "nvm: installed node $nvmrc_version"
+            end
         end
     end
 end
